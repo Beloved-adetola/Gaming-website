893e43f78c5467223d699fc30a6f974e2f4a6287
Adetola Beloved <belovedadetola@gmail.com>
2024-11-16 12:16:58 +0100

    Initial commit

---
+++ Animated cursor/cursor.js
@@ -0,0 +1,20 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    // cursorOutline.style.left = `${posX}px`;
+    // cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        top: `${posY}px`,
+        left: `${posX}px`,
+    }, {duration: 100, fill: "forwards"});
+
+})
